9fa0ccfdbe89a5776004db0c81a08fa8e34f4acb
Leonardo Galdino <leonardogaldino721@gmail.com>
2022-12-10 21:01:52 -0300

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+// Form
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
+// Tarefas
+
+// import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+    tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novaTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novaTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novaTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novaTarefas],
+        index: -1,
+      });
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  };
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novaTarefas = [...tarefas];
+    novaTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novaTarefas],
+    });
+  };
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            <li key={Math.random()}>
+              {tarefa}
+              <span>
+                <FaEdit onClick={(e) => this.handleEdit(e, index)} className="edit" />
+                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className="delete" />
+              </span>
+            </li>
+          ))}
+
+        </ul>
+      </div>
+    );
+  }
+}
